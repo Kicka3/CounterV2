9fd506829efccece9d1773e9abf0d71d1a313728
Kirill <kirill_ozornin@mail.ru>
2023-10-22 20:43:25 +0200

    Add settings1

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import {Button} from "../button/Button";
 
 export const Settings = () => {
+    // const [number, setNnumber] = useState<number>(0);
+    // const [maxValue, setMaxValue] = useState<boolean>(false);
+
+    const incBtn = () => {
+
+    }
+
+    const maximunValue = () => {
+
+    }
+
+    const resetCount = () => {
+
+
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className={"SettingsWrapper"}>
+                <div className={"DisplaySettings"}>
+                    <h1 className={"MaxValue"}>max value</h1>
+                </div>
+
+                <div className={"BtnWrapper"}>
+                    <Button
+                        name={"inc"}
+                        onClick={incBtn}
+                        maxValue={maxValue}
+                        className={maxValue ? 'DisabledBtn' : 'ActiveBtn'}
+                        disabled={maxValue}
+                    />
+                </div>
+            </div>
+        </>
+
     );
 };
 
