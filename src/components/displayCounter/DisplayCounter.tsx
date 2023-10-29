d74f49d91d322e662c2808a059be3f07b0504c8e
Kirill <kirill_ozornin@mail.ru>
2023-10-29 13:50:20 +0200

    Current version v2

--- src/components/displayCounter/DisplayCounter.tsx
+++ src/components/displayCounter/DisplayCounter.tsx
@@ -13,17 +13,11 @@ export const DisplayCounter: React.FC<DisplayCounterPropsType> = ({disabled, cur
     let statusErr = (startValue < 0 || maxValue <= startValue)
     console.log('statusErr: ' + statusErr)
 
-    // let maxFromStorage = localStorage.getItem('maxValue')
-    // let startFromStorage = localStorage.getItem('startValue')
-    //  const setMessage = !maxFromStorage ||  !startFromStorage || ( maxFromStorage &&startFromStorage && (+maxFromStorage !== maxValue ||  +startFromStorage !== startValue))
-    // console.log(!!setMessage)
-
     const displayCounter: JSX.Element =
             <h1 className={currentValue === maxValue ? "MaxNumberError" : ''}>
                 {disabled
                     ? <span className={statusErr ? "MaxNumErrorTitle" : ""}>{currentValue}</span>
                     :
-                    // <span className={statusErr ? 'errorMessage' : ""}>{currentValue === 0  ? 'Enter values and press set' : (statusErr ? 'Incorrect value' : currentValue)} </span>
                     <span className={statusErr ? 'errorMessage' : ""}>{ statusErr ? 'Incorrect value' : !status ? 'Enter values and press set' : currentValue} </span>
                 }
             </h1>
